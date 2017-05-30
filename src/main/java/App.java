b7867ba78f5e46c5adec934765e360039b97c5cd
Kevin Dinh <kevtandinh@gmail.com>
2017-05-30 13:29:04 -0400

    replaced for tri for loop with fancy equation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,11 +4,7 @@
 public class App {
 
 	public static int Tri(int n) {
-		int res = 0;
-		for (int i = n; i > 0; i--) {
-			res += i;
-		}
-		return res;
+		return (n*(n+1))/2;
 	}
 
 	public static int Lazy(int n) {
